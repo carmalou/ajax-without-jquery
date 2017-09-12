985a95825eedae2c096577e9801f7b74bbad4de8
Carmen Long <carmen.long@hobbylobby.com>
2017-09-12 16:10:00 -0500

    first commit

---
+++ HelloWordTest/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWordTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RespondToAjax()
+        {
+            return Json("Hello World!", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult PostFromAjax(TestModel postParams)
+        {
+            // do something with data, probably create db record
+            return Json("Success!");
+        }
+
+        public class TestModel
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string FavoriteColor { get; set; }
+        }
+    }
+}
